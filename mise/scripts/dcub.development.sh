8b9e1a7d3dbe5f9291d3e04c28e472271d7015c9
schadenfred <fred.schoeneman@gmail.com>
2024-04-26 20:46:11 -0700

    Copies most of mise from story_store

--- mise/scripts/dcub.development.sh
+++ mise/scripts/dcub.development.sh
@@ -2,8 +2,8 @@
 
 dcd
 
-. mise/scripts/docker-prune-club.sh 
+. mise/scripts/docker.prune.sh 
+. mise/scripts/docker.info.sh 
 # . mise/scripts/docker-prune-all.sh 
 
 dc build --with-dependencies builder-development
-dc up 
